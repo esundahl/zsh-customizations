8fa8fe2e6f6c76dabec47cf79c2813d2de4ecccd
Erik Sundahl <erik@sundahl.io>
2020-07-29 20:27:51 -0700

    adds aliases file

---
+++ aliases.zsh
@@ -0,0 +1,41 @@
+# Navigation Aliases
+##################################
+alias ll="ls -loaGh"
+alias prj='cd ~/projects'
+alias bashp='vim ~/.bash_profile'
+alias vimrc='vim ~/.dotfiles/.vimrc'
+alias vimcs='vim ~/Dropbox/settings/vim/cheat_sheet.md'
+alias notes='vim ~/Dropbox/notes/'
+alias v='vim'
+alias ..='cd ..'
+alias h='cd ~'
+alias j='jump'
+alias jh='jump home'
+alias jc='jump code'
+alias jp='jump projects'
+alias jj='jump janus'
+alias sim='open /Applications/Xcode.app/Contents/Applications/iPhone\ Simulator.app'
+alias m='make'
+##################################
+
+# Git Aliases
+##################################
+alias g="git status"
+alias ga='git add'
+alias gb='git branch'
+alias gba='git branch -a'
+alias gc='git commit'
+alias gca='git commit -v -a'
+alias gco='git checkout'
+alias gcod='git checkout develop'
+alias gcom='git checkout master'
+alias gd='git diff | vimdiff'
+alias gf="git fetch"
+alias gl='git lg'
+alias gm="git merge"
+alias gp="git push"
+alias gpod="git push origin develop"
+alias gpom="git push origin master"
+alias grl="git reflog"
+alias gs='git status'
+##################################
